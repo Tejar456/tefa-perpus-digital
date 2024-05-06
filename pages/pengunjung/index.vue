ba0133bff609362a84c13b30be0b44b40e9f53c4
Tejar Maulana <142279980+Tejar456@users.noreply.github.com>
2024-05-06 10:29:41 +0700

    update pages

--- pages/pengunjung/index.vue
+++ pages/pengunjung/index.vue
@@ -3,7 +3,7 @@
     <div class="row">
       <div class="col-lg-12 p-5">
         <h2 class="text-center my-4">RIWAYAT KUNJUNGAN</h2>
-        <form @submit.prevent="getPengunjung">
+        <form @submit.prevent="cariPengunjung">
           <div class="my-3">
             <input
               v-model="keyword"
@@ -53,10 +53,9 @@ const keyword = ref("");
 
 const getPengunjung = async () => {
   const { data, error } = await supabase
-    .from("pengunjung")
-    .select(`*, keanggotaan(*), keperluan(*)`)
-    // .order("id", { ascending: false });
-    .ilike("nama", `%${keyword.value}%`);
+  .from("pengunjung")
+  .select(`*, keanggotaan(*), keperluan(*)`)
+  .order("id", { ascending: false });
   if (data) visitors.value = data;
 };
 
@@ -67,13 +66,14 @@ const countVisitor = async () => {
   if (data) visitor.value = count;
 };
 
-// const cariPengunjung = async () => {
-//   const { data } = await supabase
-//     .form("pengunjung")
-//     .select(`*, keanggotaan(*), keperluan(*)`)
-//     .ilike("nama", `%${keyword.value}%`);
-//   if (data) visitors.value = data;
-// };
+const cariPengunjung = async () => {
+  const { data, error } = await supabase
+  .from("pengunjung")
+  .select(`*, keanggotaan(*), keperluan(*)`)
+  .ilike("nama", `%${keyword.value}%`);
+  if (data) visitors.value = data;
+};
+
 
 onMounted(() => {
   getPengunjung();
